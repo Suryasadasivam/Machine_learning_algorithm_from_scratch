47f92180ff6273b419ad5b56a148bce3dbd01fbd
Suryasadasivam <suryasadasivamm@gmail.com>
2025-01-14 17:13:49 +0530

    Linear regression data generated

---
+++ Linear_regression_data.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from linearregression import LinearRegression
+
+X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+
+#fig = plt.figure(figsize=(8,6))
+#plt.scatter(X[:, 0], y, color = "b", marker = "+", s = 30)
+#plt.show()
+
+reg = LinearRegression(lr=0.0001)
+reg.fit(X_train,y_train)
+predictions = reg.predict(X_test)
+
+def mse(y_test, predictions):
+    return np.mean((y_test-predictions)**2)
+
+mse = mse(y_test, predictions)
+print(mse)
